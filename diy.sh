b2ee073a31363e8614cee48616a0bab8368b2a79
Chauncey09 <33027599+chauncey09@users.noreply.github.com>
2020-03-06 16:03:42 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -11,3 +11,7 @@ sed -i 's/192.168.1.1/192.168.2.10/g' package/base-files/files/bin/config_genera
 #添加软件包
 git clone https://github.com/BoringCat/luci-app-mentohust.git package/luci-app-mentohust
 git clone https://github.com/KyleRicardo/MentoHUST-OpenWrt-ipk.git package/MentoHUST-OpenWrt-ipk
+
+#更新
+./scripts/feeds update -a
+./scripts/feeds install -a
